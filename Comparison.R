fc7308391ced19c8940fd15496ca688ad21f5a2b
eringutbrod <eringutbrod@gmail.com>
2022-06-22 11:15:48 -0400

    EMG edits

--- Comparison.R
+++ Comparison.R
@@ -9,6 +9,8 @@ library(lubridate)
 library(rstatix)
 library(knitr) 
 
+# EMG comment - it's bad practice to just load libraries that you don't use
+
 jasmine_angle_data=read.csv('Data/Sounders_Health_Assessment_Jasmine.csv',
                             col.names=c("AoD_J","offset_J","AoD_K","offset_K","julian","ID","year"))
 
@@ -21,6 +23,19 @@ class(jasmine_angle_data$AoD_J)
 
 t.test(jasmine_angle_data$AoD_J~jasmine_angle_data$AoD_K, paired=T)
 
+# EMG's code
+# I looked up t.test in the help file and read the part about "If paired is TRUE" under the Details section
+# This made me realize you need to specify x and y explicitly (rather than using the formulaic method with ~)
+t.test(x=jasmine_angle_data$AoD_J, y=jasmine_angle_data$AoD_K, paired=T)
+# p-value is not significant which is good - there is not a significant difference in means
+# between the Jasmine and Kira
+
+cor(x=jasmine_angle_data$AoD_J, y=jasmine_angle_data$AoD_K, use="complete.obs")
+# it's a nice correlation, tho I suppose you'd be hoping for higher.
+# this value may improve with more measurements (higher sample size)
+
+plot(x=jasmine_angle_data$AoD_J, y=jasmine_angle_data$AoD_K)
+# scatter plot shows good agreement btwn analysts
 
 
 
